3de9f6e97c1153d41652dab8f53b469f72ec1105
scott <chris.c.scott@outlook.com>
2018-08-24 17:50:32 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,36 @@
+const fetch = require("node-fetch");
+
+const url = "https://api.groupme.com/v3/bots/post"
+const id = "c05eb2fde22c2add38246cea3f"
+
+const postText = async (url, id) => {
+  try {
+    data = {
+      'bot_id': id,
+      'text' : "Sameple text"
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data)
+    });
+
+    const json = await response.json();
+    console.log(json)
+
+}catch (error) {
+    console.log(error);
+  }
+
+};
+
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]), botRegex = /^\/coolguy$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    postText(url, id);
+  } else {
+    console.log("don't care");
+  }
+
+}
